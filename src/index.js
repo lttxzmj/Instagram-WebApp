0b5746b41c7baf172f173772d823ca56b09f00ca
Shoucheng H <h.shoucheng@gmail.com>
2017-11-01 17:57:40 +0800

    Install Webpack and rewrite with react

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+function App() {
+    return (< div > hello world < /div>)
+}
+
+const root = document.getElementById('root');
+ReactDOM.render( <App / >, root)
