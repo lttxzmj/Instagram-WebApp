4f66323225738aac96347c02ac5b5072a82666f5
Shoucheng H <h.shoucheng@gmail.com>
2017-11-01 19:46:38 +0800

    Change gitignore and delete unnecessary section of html

--- src/index.jsx
+++ src/index.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 function App() {
-    return (< div > hello world < /div>)
+	return (
+		<div>
+      hello world
+    </div>
+	);
 }
-
 const root = document.getElementById('root');
-ReactDOM.render( <App / >, root)
+ReactDOM.render(<App /> , root)
