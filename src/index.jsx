ba420df3b6ab8dc58d64c452d1fb1c2523f6b040
Shoucheng H <h.shoucheng@gmail.com>
2017-11-02 15:34:45 +0800

    Change ignore and tidy index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-function App() {
-	return (
-		<div>
-      hello world
-    </div>
-	);
+function App(){
+  return (
+    <div>hello world</div>
+  );
 }
+
 const root = document.getElementById('root');
 ReactDOM.render(<App /> , root)
