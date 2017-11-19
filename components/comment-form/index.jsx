40f846a12f1d8b615029f194dbc38ee02a236ee0
Shoucheng H <h.shoucheng@gmail.com>
2017-11-19 16:47:33 +0800

    Initial build comment-form component

---
+++ components/comment-form/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+
+export default class CommentForm extends React.Component {
+  render(){
+    return(
+      <div>CommentForm</div>
+    )
+  }
+}
