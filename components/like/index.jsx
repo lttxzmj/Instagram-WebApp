6f12595f415f5032993e12e3b77a97da6682ce60
Shoucheng H <h.shoucheng@gmail.com>
2017-11-19 16:45:24 +0800

    Initial build like component

---
+++ components/like/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+
+export default class Like extends React.Component {
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    return(
+      <div>Like</div>
+    )
+  }
+}
