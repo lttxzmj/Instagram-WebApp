5fb0f4e75b6156b67c2badbb8f0e36dfd14e7e87
Shoucheng H <h.shoucheng@gmail.com>
2017-11-19 16:46:58 +0800

    Initial build comment-list component

---
+++ components/comment-list/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+
+export default class CommentList extends React.Component {
+  render(){
+    return(
+      <div>commentlist</div>
+    )
+  }
+}
