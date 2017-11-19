84878557dcdb760b56138592a77cfab476387024
Shoucheng H <h.shoucheng@gmail.com>
2017-11-19 16:44:34 +0800

    import comment-content component.

--- components/comment-box/index.jsx
+++ components/comment-box/index.jsx
@@ -1,17 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
+import CommentContent from '../comment-content/index.jsx'
 
-function CommentBox() {
-  const bgURL ="https://cl.ly/3m2k410K1H32/pic01.jpg";
-  return (
-    <div className="comment-box">
-      <div className="pic-container">
-        <img src={bgURL} alt="city in the night" />
+export default class CommentBox extends React.Component {
+  constructor(props){
+    super(props);
+  }
+  
+  render(){
+    return (
+      <div className="comment-box">
+        <div className="pic-container">
+          <img src="https://cl.ly/3m2k410K1H32/pic01.jpg" alt="city in the night" />
+        </div>
+        <CommentContent />
       </div>
-
-    </div>
-  );
+    )
+  }
 }
-
-export default CommentBox;
