6cec373d449303830eb218a08b87265f03b44280
Shoucheng H <h.shoucheng@gmail.com>
2017-11-20 18:54:34 +0800

    Add form and delete index.css

--- components/comment-form/index.jsx
+++ components/comment-form/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
 
 export default class CommentForm extends React.Component {
   render(){
     return(
-      <div>CommentForm</div>
+      <div className="comment-box center">
+        <input type="text" placeholder="Write a comment..." />
+      </div>
     )
   }
 }
