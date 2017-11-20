3bd2a83c2c1b3402aa13e937baad7647011ec412
Shoucheng H <h.shoucheng@gmail.com>
2017-11-20 18:55:20 +0800

    Add comment item list and delete index.css

--- components/comment-list/index.jsx
+++ components/comment-list/index.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
 
 export default class CommentList extends React.Component {
   render(){
     return(
-      <div>commentlist</div>
+      <div className="comment-detail">
+        <div className="comment-item item-1">
+          <div className="person-photo">
+            <img src="https://cl.ly/231z0K3c2A1c/avatar01.png" alt="miekd" />
+          </div>
+          <span className="person-name">miekd</span>
+          <span className="comment-content">Beautiful</span>
+        </div>
+        <div className="comment-item item-2">
+          <div className="person-photo">
+            <img src="https://cl.ly/230X330Z0X1S/avatar02.png" alt="ryangomba" />
+          </div>
+          <span className="person-name">ryangomba</span>
+          <span className="comment-content">Wow! those clouds are great!</span>
+        </div>
+      </div>
     )
   }
 }
