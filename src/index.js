c23eeb4c6d724c48d356de186f387c5860b98bb6
Shoucheng H <h.shoucheng@gmail.com>
2017-11-20 18:58:28 +0800

    Import index.css and normalize.css and fontawesome

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import CommentBox from '../components/comment-box/index.jsx'
+import '../css/normalize.css'
+import '../css/index.css'
+import '../css/mixin.sass'
 
 function App() {
   return (
