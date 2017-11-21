57b324ff60412db00372ffbc33f320cba2903a15
Shoucheng H <h.shoucheng@gmail.com>
2017-11-21 23:53:45 +0800

    Apply data to comment list and item

--- components/comment-list/index.jsx
+++ components/comment-list/index.jsx
@@ -1,24 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import ListItem from '../list-item/index.jsx'
 
 export default class CommentList extends React.Component {
+  constructor(props){
+    super(props);
+  }
+
   render(){
+    const data = this.props.data;
+    const listItem = data.map((item, index) => <ListItem item={item} key={index}/>)
+
     return(
       <div className="comment-detail">
-        <div className="comment-item item-1">
-          <div className="person-photo">
-            <img src="https://cl.ly/231z0K3c2A1c/avatar01.png" alt="miekd" />
-          </div>
-          <span className="person-name">miekd</span>
-          <span className="comment-content">Beautiful</span>
-        </div>
-        <div className="comment-item item-2">
-          <div className="person-photo">
-            <img src="https://cl.ly/230X330Z0X1S/avatar02.png" alt="ryangomba" />
-          </div>
-          <span className="person-name">ryangomba</span>
-          <span className="comment-content">Wow! those clouds are great!</span>
-        </div>
+        {listItem}
       </div>
     )
   }
