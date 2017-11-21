87161dfa5dadc28d1bd5d80c32f59f9ccc97b6f0
Shoucheng H <h.shoucheng@gmail.com>
2017-11-21 23:49:59 +0800

    Fetch data from sever end

--- components/comment-content/index.jsx
+++ components/comment-content/index.jsx
@@ -7,13 +7,36 @@ import CommentForm from '../comment-form/index.jsx'
 export default class CommentContent extends React.Component {
   constructor(props){
     super(props);
+    this.state = {
+      data: []
+    }
   }
+
+  componentDidMount(){
+    var that = this;
+    fetch('http://localhost:8000/api/comment', {
+      method: 'get'
+    }).then(function(response){
+      if (response.status !== 200) {
+        console.log('Looks like there was a problem. Status Code: ' + response.status);
+        return;
+      }
+      response.json().then(function(data){
+          that.setState({
+            data: data
+          });
+        });
+    }).catch(function(err){
+      console.log(err);
+    });
+  };
+
 
   render(){
     return(
       <div className="comment-status">
         <Like />
-        <CommentList />
+        <CommentList data={this.state.data}/>
         <CommentForm />
       </div>
     )
