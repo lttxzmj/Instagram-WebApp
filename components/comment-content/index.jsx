7732458ab9f5e57177fb775e1f07606cd6a059f4
Shoucheng H <h.shoucheng@gmail.com>
2017-11-23 18:38:46 +0800

    Get like data from server end and pass it to child component

--- components/comment-content/index.jsx
+++ components/comment-content/index.jsx
@@ -8,34 +8,52 @@ export default class CommentContent extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      data: []
+      data: [],
+      like: []
     }
   }
 
   componentDidMount(){
-    var that = this;
     fetch('http://localhost:8000/api/comment', {
       method: 'get'
-    }).then(function(response){
+    }).then((response) => {
       if (response.status !== 200) {
         console.log('Looks like there was a problem. Status Code: ' + response.status);
         return;
       }
-      response.json().then(function(data){
-          that.setState({
+      response.json().then((data) => {
+          this.setState({
             data: data
           });
         });
     }).catch(function(err){
       console.log(err);
     });
+
+    fetch('http://localhost:8000/api/likes', {
+      method: 'get'
+    }).then((response) => {
+      if (response.status !== 200) {
+        console.log('Looks like there was a problem. Status Code: ' + response.status);
+        return;
+      }
+      response.json().then((like) => {
+        console.log(like);
+          this.setState({
+            like: like
+          });
+        });
+    }).catch(function(err){
+      console.log(err);
+    });
+
   };
 
 
   render(){
     return(
       <div className="comment-status">
-        <Like />
+        <Like like={this.state.like}/>
         <CommentList data={this.state.data}/>
         <CommentForm />
       </div>
