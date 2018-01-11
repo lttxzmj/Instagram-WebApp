d7fbda2b57d34c51a7c9b898f5ec6b516b27a239
Shoucheng H <h.shoucheng@gmail.com>
2018-01-11 21:29:44 +0800

    Fetch data

--- components/like/index.jsx
+++ components/like/index.jsx
@@ -21,7 +21,81 @@ export default class Like extends React.Component {
     this.setState({
       isToggleOn: !this.state.isToggleOn
     });
+
+    //将喜欢的人提交到服务器
+    //再从服务器获取喜欢的人数，重新渲染组件的内容
+    function handlePost(event){
+      event.preventDefault();
+      var data = {
+          "id": 0,
+          "name": "string",
+          "Url": "string",
+          "likeStatus": true,
+          "createdBy": {
+            "id": 0,
+            "name": "string",
+            "avatarUrl": "string"
+          },
+          "commentsInfo": {
+            "id": 0,
+            "createdBy": {
+              "id": 0,
+              "name": "string",
+              "avatarUrl": "string"
+            },
+            "createdAt": "2017-11-24T16:40:48.408Z",
+            "content": "string"
+          }
+        };
+
+      fetch('http://localhost:8080/api/likes/like', {
+        method: 'post',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(data)
+      }).then((res) => {console.log(res)})
+    }
+    handlePost(e);
+
+    // function GetDataFromPost(){
+    //   fetch('http://localhost:8000/api/likes', {
+    //     method: 'get'
+    //   }).then((response) => {
+    //     if (response.status !== 200) {
+    //       console.log('Looks like there was a problem. Status Code: ' + response.status);
+    //       return;
+    //     }
+    //     response.json().then((like) => {
+    //       this.setState((preState) => {
+    //         num: preState.num + 1
+    //       })
+    //         console.log(like)
+    //       });
+    //   }).catch(function(err){
+    //     console.log(err);
+    //   });
+    // }
+    // GetDataFromPost();
   }
+
+  // componentDidMount(){
+  //   fetch('http://localhost:8000/api/likes', {
+  //     method: 'get'
+  //   }).then((response) => {
+  //     if (response.status !== 200) {
+  //       console.log('Looks like there was a problem. Status Code: ' + response.status);
+  //       return;
+  //     }
+  //     response.json().then((like) => {
+  //       this.setState((preState) => {
+  //         num: preState.num + 1
+  //       })
+  //         console.log(like)
+  //       });
+  //   }).catch(function(err){
+  //     console.log(err);
+  //   });
+  // }
+
 
   render(){
     return(
